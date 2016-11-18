24d57abfea08740fdb4279357965178e91708c2d
Ryanryanjp <kidamaru@gmail.com>
2016-11-18 20:11:15 +0000

    フゥ♪フゥ♪

--- fizz.py
+++ fizz.py
@@ -5,7 +5,7 @@ for count in range(1, 101):
 	if (count % 3 == 0) and (count % 5 == 0):
 		print('fizz buzz')
 
-	elif count % 3 == 0:
+	elif (count % 3 == 0):
 		print ('fizz')
 
 	elif count % 5 == 0:
