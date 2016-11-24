bbe8e78853e914b161e2b7d559aec29a22fb9fc0
Ryanryanjp <kidamaru@gmail.com>
2016-11-24 10:16:25 +0900

    指定された数字の約数を数えるプログラム

---
+++ divisor.py
@@ -0,0 +1,12 @@
+#50の約数を求めるプログラム
+
+st = int(input('入力した整数の約数を出力します : '))
+
+count = 0
+for i in range(1, st + 1):
+
+  if (st % i == 0):
+    print (i)
+    count += 1
+
+print ('{0}の約数は{1}こありました'.format(st, count))
