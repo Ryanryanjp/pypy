2e3bafa659aa68b358a3f6783bb1199294d95fd8
Ryanryanjp <kidamaru@gmail.com>
2016-11-26 14:33:54 +0900

    タイマー試作

--- time.py
+++ time.py
@@ -5,19 +5,19 @@ import time
 
 count = 0
 sec = 0
-man = 0
+miu = 0
 hor = 0
 
 while (count < 1):
   
-  print ('{0}:{1}:{2}'.format(hor, man, sec), '\r', end="")
+  print ('{0}:{1}:{2}'.format(hor, miu, sec), '\r', end="")
   time.sleep(1)
   sec += 1
 
   if (sec == 60):
-    man += 1
+    miu += 1
     sec = 0
 
-  if (man == 60):
+  if (miu == 60):
     hor += 1
-    man = 0
+    miu = 0
